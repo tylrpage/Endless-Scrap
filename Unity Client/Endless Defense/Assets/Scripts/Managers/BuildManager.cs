474aa3374761159dd8b740eeb3089138feac8823
Tyler Page <tyler@robotogames.com>
2022-01-31 09:19:03 -0800

    Clicking on ui while buildings cancels the building

--- Unity Client/Endless Defense/Assets/Scripts/Managers/BuildManager.cs
+++ Unity Client/Endless Defense/Assets/Scripts/Managers/BuildManager.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Linq;
 using Pathfinding;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Point = Pathfinding.Point;
 
 public class BuildManager : MonoBehaviour
@@ -258,11 +259,47 @@ public class BuildManager : MonoBehaviour
 
     private bool upAfterBuildButtonClicked;
     private Vector2? dragBeginSnappedPosition;
+    
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    public bool IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+ 
+ 
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    private bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == 5)
+                return true;
+        }
+        return false;
+    }
+ 
+ 
+    //Gets all event system raycast results of current mouse or touch position.
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
 
     private void Update()
     {
         if (_selectedBuilding != null && _camera != null && upAfterBuildButtonClicked)
         {
+            bool onUI = false;
+            if (IsPointerOverUIElement())
+            {
+                onUI = true;
+                Debug.Log("onui");
+            }
             // Snap to grid under mouse
             Vector2 mouseWorldPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
             int selectedBuildSizeX = (int) Mathf.Floor(_selectedBuilding.Size.x);
@@ -340,7 +377,11 @@ public class BuildManager : MonoBehaviour
 
             if (!CameraControls.IsDragging())
             {
-                if (Input.GetMouseButtonDown(0))
+                if (Input.GetMouseButtonDown(0) && onUI)
+                {
+                    ClearSelection();
+                }
+                else if (Input.GetMouseButtonDown(0))
                 {
                     dragBeginSnappedPosition = snappedPosition;
                 }
